75f12820c7fa06f82641674542661dc60e1cc73d
Maiquel Mojica <maiquel.mojica@gmail.com>
2021-10-25 13:36:42 -0400

    Agregado component de ejemplo

---
+++ HBRPractNet5/Components/UsuariosComponent.cs
@@ -0,0 +1,32 @@
+﻿using HBRPractNet.Models;
+using HBRPractNet.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HBRPractNet.Components
+{
+    public class UsuariosComponent : ViewComponent
+    {
+        private HBR_PracticaContext _context;
+        public UsuariosComponent(HBR_PracticaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<UsuariosViewModel> usuarios = new List<UsuariosViewModel>();
+
+            foreach (Usuario usuario in _context.Usuarios.ToList())
+            {
+                usuarios.Add(new UsuariosViewModel()
+                { Usuario = usuario });
+            }
+            return View(usuarios);
+        }
+    }
+}
--- HBRPractNet5/Controllers/LanguageController.cs
+++ HBRPractNet5/Controllers/LanguageController.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading;
-using System.Web;
 
 namespace HBRPractNet.Controllers
 {
